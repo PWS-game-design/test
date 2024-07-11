363742d402e69328822e52cde629dd6315835b64
MautjeH <mhw.hegeman@gmail.com>
2024-07-11 22:45:52 +0200

    added a deathcount

---
+++ test/Assets/scripts/deathcount.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class deathcount : MonoBehaviour
+
+{
+    int count = 0;
+
+public Text ValueText;
+[SerializeField] private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if(rb.transform.position.y < -20f)
+        {
+            count = count + 1;
+            ValueText.text = count.ToString();
+        }  
+    }
+}
