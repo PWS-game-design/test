dcad55a9781deeb8d6afa8efc4f69a49275bb450
FrostyMentosBox <jay.ahne@outlook.com>
2024-07-12 21:25:24 +0200

    moved the Collin (gonna work on making em move on thier own)
    COLLINPATROL SCRIPT IS NOG NIET AF

---
+++ test/Assets/scripts/CollinPatrol.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollinPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - Transform.position;
+    }
+}
