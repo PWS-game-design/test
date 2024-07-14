5aed79bb436889947982327d6eed72cdeea54d5d
MautjeH <mhw.hegeman@gmail.com>
2024-07-14 22:12:59 +0200

    settings bug fix

--- test/Assets/scripts/PauseMenu.cs
+++ test/Assets/scripts/PauseMenu.cs
@@ -9,30 +9,33 @@ public class pausemenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public static bool isPaused;
+    static bool settings;
     // Start is called before the first frame update
     void Start()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
-
+        settings = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Escape))
-       {
-            if(!isPaused)
+        if(Input.GetKeyDown(KeyCode.Escape) && settings == false)
+        {
+            if(isPaused)
             {
-                PauseGame();
+                Debug.Log("resume");
+                ResumeGame();
             }
             else
             {
-                ResumeGame();
+                Debug.Log("Pause");
+                PauseGame();
             }
-            
-       } 
+                
+        }
     }
 
 
@@ -55,5 +58,18 @@ public class pausemenu : MonoBehaviour
     {
         Debug.Log("quit");
         Application.Quit();
+    }
+    public void settingsmenu()
+    {
+        if(settings == true)
+        {
+            Debug.Log("false");
+            settings = false;
+        }
+        else
+        {
+            Debug.Log("true");
+            settings = true;
+        }
     }
 }
