8d13b5913ee289969a74f95200f4ceb045293285
FrostyMentosBox <jay.ahne@outlook.com>
2024-08-22 16:41:28 +0200

    collin kan lopen (uit zichzelf

--- test/Assets/scripts/CollinPatrol.cs
+++ test/Assets/scripts/CollinPatrol.cs
@@ -8,12 +8,13 @@ public class CollinPatrol : MonoBehaviour
     public GameObject pointB;
     private Rigidbody2D rb;
     private Transform currentPoint;
-    {
-
-    }
+    Vector2 orginalPosition;
+    [SerializeField] private Transform OGP;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
+        orginalPosition = new Vector2(rb.transform.OGP.x, rb.transform.OGP.y);
         rb = GetComponent<Rigidbody2D>();
         currentPoint = pointB.transform;
     }
@@ -21,6 +22,33 @@ public class CollinPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 point = currentPoint.position - Transform.position;
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+         if(rb.transform.position.y < -10f)
+        {
+            rb.transform.position = orginalPosition;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 }
